aa8a004d7932a6ea67cac0e416bf52a09b3f669a
azamatshauenbekov <bravlstarstop05@mail.ru>
2023-10-20 10:14:50 +0600

    All tasks solved

--- javaaa/src/week1/BanknotesAndCoins.java
+++ javaaa/src/week1/BanknotesAndCoins.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class BanknotesAndCoins {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double n = input.nextDouble();
-
-        if (n < 0) {
+        double n = input.nextDouble() * 100;
+        int m = (int) n;
+        if (n > 0) {
             double[] balance = {100.00, 50.00, 20.00, 10.00, 5.00, 2.00, 1.00, 0.50, 0.25, 0.10, 0.5, 0.01};
-            for (double i : balance) {
-                System.out.println("Notes: " + n / i + " nota(s) de R$ " + i);
-                n = n % i;
+            int[] balance2 = {100 * 100, 50 * 100, 20 * 100, 10 * 100, 5 * 100, 2 * 100, 100, 50, 25, 10, 5, 1};
+
+            for (int i = 0; i < balance2.length; i++) {
+                System.out.println("Notes: " + (m / balance2[i]) + " nota(s) de R$ " + balance[i]);
+                m = m % balance2[i];
             }
         }
     }
